148fb26cc9d4d79522de88a88bf1dec913facf4f
yarenty <yarenty@gmail.com>
2017-04-28 10:19:13 +0100

    data prep

---
+++ src/main/scala/com/yarenty/ai/timeseries/PrepareData.scala
@@ -0,0 +1,11 @@
+package com.yarenty.ai.timeseries
+
+/**
+  * Created by yarenty on 28/04/2017.
+  */
+object PrepareData extends App {
+
+
+  val initialData = Array(7,7,7,6,5,3,3,4,4,5,6,7,8,9,9,9,10,9,9,8,7,6,6,5)
+
+}
